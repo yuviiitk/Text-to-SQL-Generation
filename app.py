4ab9b7a808c662486c81b241276376beec987742
Yuvraj Singh Kharte <yuvrajkharte01@email.com>
2026-03-25 13:15:00 +0530

    Phase 3: Groq with API

--- app.py
+++ app.py
@@ -1,1 +1,47 @@
-This is test
+import streamlit as st
+import pandas as pd
+import sqlite3
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key = api_key)
+
+def generate_sql(prompt):
+    response = client.chat.completions.create(model="llama-3.1-8b-instant",
+                                              messages = [{"role": "user", "content": prompt}])
+    return response.choices[0].message.content
+
+st.title("Text to SQL Generation")
+
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    conn = sqlite3.connect("data.db")
+    df.to_sql("my_table", conn, if_exists="replace", index=False)
+    st.success("Data stored in database succesfully")
+    st.subheader("Data Preview")
+    st.dataframe(df.head())
+    columns = ", ".join(df.columns)
+    st.subheader("Table Schema ( Columns )")
+    st.write(columns)
+    st.subheader("Ask a question")
+    user_query = st.text_input("Enter your question in english")
+    if user_query:
+        prompt = f"""
+                You are an SQL expert
+                Table: my_table
+                Columns: {columns}
+                Rules:
+                    - Use only given columns
+                    - Return only SQL query
+                    - No explanation
+                Question: {user_query}
+                """
+        sql_query = generate_sql(prompt)
+        st.subheader("Generated SQL")
+        st.code(sql_query, language="sql")
